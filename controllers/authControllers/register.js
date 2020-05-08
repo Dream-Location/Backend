d567f0606d9b802c8fbb9a9135b1f499911ed1a5
DimejiAre <olardi2@gmail.com>
2020-05-08 15:59:40 +0100

    fix: add try catch block to register controller

--- controllers/authControllers/register.js
+++ controllers/authControllers/register.js
@@ -5,20 +5,29 @@ const User = require('../../models/user')
 module.exports = async function(req, res){
     const {username, password} = req.body
 
-    let user = await User.findOne({where: { username}})
+    try{
 
-    if (user){
-        return res.status(400).json({message: "user already exists"})
+        let user = await User.findOne({where: { username}})
+
+        if (user){
+            return res.status(400).json({message: "user already exists"})
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        
+        user = await User.create({ username, password: encryptedPassword })
+
+        const token = await generateToken(username)
+
+        return res.status(201).json({
+            message : `welcome ${user.username}`,
+            token: token
+        })
+    } catch(err) {
+        return res.status(401).json({
+            message : ("registration failed", err.message)
+        })
     }
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
     
-    user = await User.create({ username, password: encryptedPassword })
-
-    const token = await generateToken(username)
-
-    return res.status(201).json({
-        message : `welcome ${user.username}`,
-        token: token
-    })
 }
