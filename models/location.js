6c01120618cf6d631e1aa655ac95b58f52c5e6b0
DimejiAre <olardi2@gmail.com>
2020-05-12 00:38:08 +0100

    feat: create location model

---
+++ models/location.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+const sequelize = require('./index')
+
+const Location = sequelize.define('location', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    country: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Location
