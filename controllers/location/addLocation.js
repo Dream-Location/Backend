4e72e390976e024385d643f8881b3e41489387b2
DimejiAre <olardi2@gmail.com>
2020-05-12 00:38:41 +0100

    feat: add controller to add locations

---
+++ controllers/location/addLocation.js
@@ -0,0 +1,31 @@
+const Location = require('../../models/location')
+
+module.exports = async function (req, res) {
+    try {
+        const locationData = req.body
+        console.log(locationData)
+        const { description, imageUrl, city, country, price, rating } = locationData
+
+        let location = await Location.findOne({ where: { imageUrl, city, country, price } })
+        if (location) return res.status(400).json({ message: "Location already exists" })
+
+        location = await Location.create({
+            description,
+            imageUrl,
+            city,
+            country,
+            price,
+            rating
+        })
+
+        return res.status(201).json({
+            location
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            message : ("Error occurred when creating location", err.message)
+        })
+    }
+    
+}
