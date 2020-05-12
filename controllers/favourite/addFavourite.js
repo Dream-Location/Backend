3d6f62a07e5d8e99407d703d16ecce45e20f3c12
DimejiAre <olardi2@gmail.com>
2020-05-12 20:40:39 +0100

    feat: create controller for adding favourite

---
+++ controllers/favourite/addFavourite.js
@@ -0,0 +1,23 @@
+const { User } = require('../../models')
+
+module.exports = async function (req, res) {
+    try {
+        const userId = req.user.subject
+        const { location } = req.body
+        const user = await User.findOne({ where: { id: userId } })
+        let response = await user.addLocation(location)
+        if (!response){
+            return res.status(401).json({
+                message: "Already added to favourites"
+            })
+        }
+        return res.status(201).json({
+            message: response
+        })
+    } catch(err){
+        return res.status(500).json({
+            message: "Error occurred adding favourites, " + err.message
+        })
+    }
+    
+}
