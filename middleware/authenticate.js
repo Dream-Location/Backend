2c1b199ca98a3c1d1b68319bf1e203fdef1691b7
DimejiAre <olardi2@gmail.com>
2020-05-12 20:39:44 +0100

    fix: update authenticate function

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,25 +1,27 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = async function (req, res, next){
-    let token = ""
+module.exports = async function (req, res, next) {
+    try {
+        const token = req.headers.authorization;
 
-    if (req.headers.hasOwnProperty('authorization')){
-        token = req.headers['authorization']
-    }
+        if (!token) {
+            return res.status(400).json({
+                message: "provide token"
+            })
+        }
 
-    if (!token){
-        res.status(400).json({
-            message: "provide token"
-        })
-    }
-
-    let decoded = await jwt.verify(token, process.env.JWT_SECRET);
-    if(decoded){
-        req.user = decoded
-        next()
-    } else {
-        res.status(401).json({
-            message: "invalid token"
+        let decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        if (decoded) {
+            req.user = decoded
+            next()
+        } else {
+            return res.status(401).json({
+                message: "invalid token"
+            })
+        }
+    } catch(err){
+        return res.status(400).json({
+            message: "invalid token supplied, " +  err.message
         })
     }
 }
