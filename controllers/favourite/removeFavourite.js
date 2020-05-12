918a679d08442475d17e025bfe1e8f2d0433ff20
DimejiAre <olardi2@gmail.com>
2020-05-12 20:41:13 +0100

    feat: create controller for removing favourite

---
+++ controllers/favourite/removeFavourite.js
@@ -0,0 +1,23 @@
+const { User } = require('../../models')
+
+module.exports = async function (req, res) {
+    try {
+        const userId = req.user.subject
+        const location = req.params.id
+        const user = await User.findOne({ where: { id: userId } })
+        let response = await user.removeLocation(location)
+        if (!response){
+            return res.status(404).json({
+                message: "Already removed from favourites"
+            })
+        }
+        return res.status(200).json({
+            message: response
+        })
+    } catch(err){
+        return res.status(500).json({
+            message: "Error occurred removing from favourites, " + err.message
+        })
+    }
+    
+}
